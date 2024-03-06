7188ede3411c94aa9d248f3c1aa42f57268e7cc4
Fabio Pliger <fpliger@anaconda.com>
2024-03-06 13:17:39 -0500

    add media and devices access

--- local-fs/main.py
+++ local-fs/main.py
@@ -1,8 +1,11 @@
 from files import *
-from pyscript import window
+from pyscript import window, display
+import media
 
 my_files = Files()
 
 async def get_file(event):
     data = await my_files.get_file_contents("test.txt")
-    window.console.log(data)
+    display(data)
+
+await media.list_media_devices()
