ff4d504f968da1842c5a00896d36c6041ec6eee8
Jan Burgy <jburgy@gmail.com>
2025-10-28 07:55:50 -0400

    Add dc.js example

---
+++ dc/README.md
@@ -0,0 +1,21 @@
+# dc.js - Dimensional Charting Javascript Library
+
+[**dc.js**](https://dc-js.github.io/dc.js/) s a javascript charting library with
+native  [crossfilter](http://crossfilter.github.io/crossfilter/) support, 
+allowing highly efficient exploration on large multi-dimensional datasets 
+(inspired by crossfilter's demo). It leverages  [d3](http://d3js.org/) to 
+render charts in CSS-friendly SVG format. Charts rendered using dc.js are 
+data driven and reactive and therefore provide instant feedback to user 
+interaction. 
+
+
+## Libraries Used
+
+- [dc.js](https://dc-js.github.io/dc.js/docs/html/)
+- [crossfilter](https://github.com/crossfilter/crossfilter/wiki/API-Reference)
+- [D3](https://d3js.org/api)
+
+## References
+
+- [Original source](https://dc-js.github.io/dc.js/docs/stock.html)
+- [Pyodide - to_js](https://pyodide.org/en/stable/usage/api/python-api/ffi.html#pyodide.ffi.to_js)
---
+++ dc/main.py
@@ -0,0 +1,313 @@
+import datetime as dt
+import io
+from math import floor, isnan
+
+import js
+import pandas as pd
+from pyodide.ffi import create_proxy, to_js
+from pyodide.http import pyfetch
+
+
+def year_add(p, v, _nf):
+    p["count"] += 1
+    p["absGain"] += v.close - v.open
+    p["fluctuation"] += abs(v.close - v.open)
+    p["sumIndex"] += (v.open + v.close) / 2
+    p["avgIndex"] = p["sumIndex"] / p["count"]
+    p["percentageGain"] = (p["absGain"] / p["avgIndex"]) * 100 if p["avgIndex"] else 0
+    p["fluctuationPercentage"] = (
+        (p["fluctuation"] / p["avgIndex"]) * 100 if p["avgIndex"] else 0
+    )
+    return p
+
+
+def year_remove(p, v, _nf):
+    p["count"] -= 1
+    p["absGain"] -= v.close - v.open
+    p["fluctuation"] -= abs(v.close - v.open)
+    p["sumIndex"] -= (v.open + v.close) / 2
+    p["avgIndex"] = p["sumIndex"] / p["count"] if p["count"] else 0
+    p["percentageGain"] = (p["absGain"] / p["avgIndex"]) * 100 if p["avgIndex"] else 0
+    p["fluctuationPercentage"] = (
+        (p["fluctuation"] / p["avgIndex"]) * 100 if p["avgIndex"] else 0
+    )
+    return p
+
+
+def year_initialize():
+    return {
+        "count": 0,
+        "absGain": 0.0,
+        "fluctuation": 0.0,
+        "fluctuationPercentage": 0.0,
+        "sumIndex": 0.0,
+        "avgIndex": 0.0,
+        "percentageGain": 0.0,
+    }
+
+
+def month_add(p, v, _nf):
+    p["days"] += 1
+    p["total"] += (v.open + v.close) / 2
+    p["avg"] += round(p["total"] / p["days"])
+    return p
+
+
+def month_remove(p, v, _nf):
+    p["days"] -= 1
+    p["total"] -= (v.open + v.close) / 2
+    p["avg"] -= round(p["total"] / p["days"]) if p["days"] else 0
+    return p
+
+
+def month_initialize():
+    return {"days": 0, "total": 0.0, "avg": 0.0}
+
+
+def gainOrLossLabel(d):
+    label = d.key
+    if js.gainOrLossChart.hasFilter() and not js.gainOrLossChart.hasFilter(label):
+        return f"{label}(0%)"
+    total = all.value()
+    if total:
+        label += f"({d.value / total * 100:.0f}%)"
+    return label
+
+
+def moveTitle(d, _):
+    value = d.value
+    value = value["avg"] if isinstance(value, dict) else value
+    if isnan(value):
+        value = 0
+    return f"{dateFormat(d.key)}\n{value:{numberFormat}}"
+
+
+js.gainOrLossChart = js.dc.PieChart.new("#gain-loss-chart")
+js.fluctuationChart = js.dc.BarChart.new("#fluctuation-chart")
+js.quarterChart = js.dc.PieChart.new("#quarter-chart")
+js.dayOfWeekChart = js.dc.RowChart.new("#day-of-week-chart")
+js.moveChart = js.dc.LineChart.new("#monthly-move-chart")
+js.volumeChart = js.dc.BarChart.new("#monthly-volume-chart")
+js.yearlyBubbleChart = js.dc.BubbleChart.new("#yearly-bubble-chart")
+js.nasdaqCount = js.dc.DataCount.new(".dc-data-count")
+js.nasdaqTable = js.dc.DataTable.new(".dc-data-table")
+
+numberFormat = ".2f"
+dateFormatSpecifier = "%m/%d/%Y"
+dateFormat = js.d3.timeFormat(dateFormatSpecifier)
+response = await pyfetch(
+    "https://raw.githubusercontent.com/dc-js/dc.js/refs/heads/develop/web-src/ndx.csv"
+)
+data = pd.read_csv(
+    io.StringIO(await response.text()), parse_dates=["date"], date_format=dateFormatSpecifier
+)
+data["day"] = data["date"].dt.strftime("%a")
+data["month"] = data["date"].dt.to_period("M").dt.to_timestamp()
+data["quarter"] = data["date"].dt.quarter
+data["year"] = data["date"].dt.year
+data["date"] = data["date"].dt.date
+
+
+def default_converter(value, _ignored1, _ignored2):
+    if isinstance(value, dt.date):
+        return js.Date.new(value.year, value.month - 1, value.day)
+    raise value
+
+ndx = js.crossfilter(to_js(data.to_dict(orient="records"), default_converter=default_converter))
+all = ndx.groupAll()
+
+yearlyDimension = ndx.dimension("year")
+yearlyPerformanceGroup = yearlyDimension.group().reduce(
+    to_js(year_add), to_js(year_remove), to_js(year_initialize)
+)
+
+dateDimension = ndx.dimension("date")
+moveMonths = ndx.dimension("month")
+monthlyMoveGroup = moveMonths.group().reduceSum(
+    to_js(lambda d: abs(d.close - d.open))
+)
+volumeByMonthGroup = moveMonths.group().reduceSum(to_js(lambda d: d.volume / 500000))
+indexAvgByMonthGroup = moveMonths.group().reduce(
+    to_js(month_add), to_js(month_remove), to_js(month_initialize)
+)
+
+gainOrLoss = ndx.dimension(
+    to_js(lambda d, *_: "Loss" if d.open > d.close else "Gain")
+)
+gainOrLossGroup = gainOrLoss.group()
+fluctuation = ndx.dimension(
+    to_js(lambda d, *_: round((d.close - d.open) / d.open * 100))
+)
+fluctuationGroup = fluctuation.group()
+quarter = ndx.dimension("quarter")
+quarterGroup = quarter.group().reduceSum(to_js(lambda d: d.volume))
+dayOfWeek = ndx.dimension("day")
+dayOfWeekGroup = dayOfWeek.group()
+
+(
+    js.yearlyBubbleChart
+    .width(990)
+    .height(250)
+    .transitionDuration(1500)
+    .margins(to_js({"top": 10, "right": 50, "bottom": 30, "left": 40}))
+    .dimension(yearlyDimension)
+    .group(yearlyPerformanceGroup)
+    .colors(js.d3.schemeRdYlGn[9])
+    .colorDomain([-500, 500])
+    .colorAccessor(to_js(lambda d, i: d.value["absGain"]))
+    .keyAccessor(to_js(lambda d: d.value["absGain"]))
+    .valueAccessor(to_js(lambda d: d.value["percentageGain"]))
+    .radiusValueAccessor(to_js(lambda d: d.value["fluctuationPercentage"]))
+    .maxBubbleRelativeSize(0.3)
+    .x(js.d3.scaleLinear().domain([-2500, 2500]))
+    .y(js.d3.scaleLinear().domain([-100, 100]))
+    .r(js.d3.scaleLinear().domain([0, 4000]))
+    .elasticY(True)
+    .elasticX(True)
+    .yAxisPadding(100)
+    .xAxisPadding(500)
+    .renderHorizontalGridLines(True)
+    .renderVerticalGridLines(True)
+    .xAxisLabel("Index Gain")
+    .yAxisLabel("Index Gain %")
+    .renderLabel(True)
+    .label(to_js(lambda d: d.key))
+    .renderTitle(True)
+    .title(
+        to_js(
+            lambda p: "\n".join(
+                [
+                    str(p.key),
+                    f"Index Gain: {p.value['absGain']:{numberFormat}}",
+                    f"Index Gain in Percentage: {p.value['percentageGain']:{numberFormat}}%",
+                    f"Fluctuation / Index Ratio: {p.value['fluctuationPercentage']:{numberFormat}}%",
+                ]
+            )
+        )
+    )
+    .yAxis()
+    .tickFormat(to_js(lambda v, *_: f"{v}%"))
+)
+(
+    js.gainOrLossChart.width(180)
+    .height(180)
+    .radius(80)
+    .dimension(gainOrLoss)
+    .group(gainOrLossGroup)
+    .label(create_proxy(gainOrLossLabel))
+)
+(
+    js.quarterChart
+    .width(180)
+    .height(180)
+    .radius(80)
+    .innerRadius(30)
+    .dimension(quarter)
+    .group(quarterGroup)
+)
+(
+    js.dayOfWeekChart
+    .width(180)
+    .height(180)
+    .margins(to_js({"top": 20, "left": 10, "right": 10, "bottom": 20}))
+    .group(dayOfWeekGroup)
+    .dimension(dayOfWeek)
+    .ordinalColors(["#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#dadaeb"])
+    .label(to_js(lambda d: d.key))
+    .title(to_js(lambda d, *_: d.value))
+    .elasticX(True)
+    .xAxis()
+    .ticks(4)
+)
+(
+    js.fluctuationChart.width(420)
+    .height(180)
+    .margins(to_js({"top": 10, "right": 50, "bottom": 30, "left": 40}))
+    .dimension(fluctuation)
+    .group(fluctuationGroup)
+    .elasticY(True)
+    .centerBar(True)
+    .gap(1)
+    .round(floor)
+    .alwaysUseRounding(True)
+    .x(js.d3.scaleLinear().domain([-25, 25]))
+    .renderHorizontalGridLines(True)
+    .filterPrinter(
+        to_js(
+            lambda filters: f"{filters[0][0]:{numberFormat}}% -> {filters[0][1]:{numberFormat}}%"
+        )
+    )
+)
+js.fluctuationChart.xAxis().tickFormat(to_js(lambda v, *_: f"{v}%"))
+js.fluctuationChart.yAxis().ticks(5)
+(
+    js.moveChart
+    .renderArea(True)
+    .width(990)
+    .height(200)
+    .transitionDuration(1000)
+    .margins(to_js({"top": 30, "right": 50, "bottom": 25, "left": 40}))
+    .dimension(moveMonths)
+    .mouseZoomable(True)
+    .rangeChart(js.volumeChart)
+    .x(js.d3.scaleTime().domain(to_js([dt.date(1985, 1, 1), dt.date(2012, 12, 31)], default_converter=default_converter)))
+    .round(js.d3.timeMonth.round)
+    .xUnits(js.d3.timeMonths)
+    .elasticY(True)
+    .renderHorizontalGridLines(True)
+    .legend(js.dc.Legend.new().x(800).y(10).itemHeight(13).gap(5))
+    .brushOn(False)
+    .group(indexAvgByMonthGroup, "Monthly Index Average")
+    .valueAccessor(to_js(lambda d, _: d.value["avg"]))
+    .stack(monthlyMoveGroup, "Monthly Index Move", to_js(lambda d, _: d.value))
+    .title(to_js(moveTitle))
+)
+(
+    js.volumeChart
+    .width(990)
+    .height(40)
+    .margins(to_js({"top": 0, "right": 50, "bottom": 20, "left": 40}))
+    .dimension(moveMonths)
+    .group(volumeByMonthGroup)
+    .centerBar(True)
+    .gap(1)
+    .x(js.d3.scaleTime().domain(to_js([dt.date(1985, 1, 1), dt.date(2012, 12, 31)], default_converter=default_converter)))
+    .round(js.d3.timeMonth.round)
+    .alwaysUseRounding(True)
+    .xUnits(js.d3.timeMonths)
+)
+(
+    js.nasdaqCount
+    .crossfilter(ndx)
+    .groupAll(all)
+    .html(to_js({
+        "some": r"<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records"
+        + " | <a href='javascript:dc.filterAll(); dc.renderAll();'>Reset All</a>",
+        "all": "All records selected. Please click on the graph to apply filters.",
+    }))
+)
+(
+    js.nasdaqTable
+    .dimension(dateDimension)
+    .section(to_js(lambda d, *_: f"{d.date.getFullYear()}/{d.date.getMonth() + 1:02d}"))
+    .size(10)
+    .columns(to_js([
+        {
+            "label": "Date",
+            "format": lambda d: dateFormat(d.date),
+        },
+        "open",
+        {
+            "label": "Change",
+            "format": lambda d: f"{d.close - d.open:{numberFormat}}",
+        },
+        "volume",
+    ]))
+    .sortBy(to_js(lambda d: d.date))
+    .order(js.d3.ascending)
+    .on(
+        "renderlet",
+        to_js(lambda table: table.selectAll(".dc-table-group").classed("info", True)),
+    )
+)
+js.dc.renderAll()
